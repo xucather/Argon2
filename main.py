212cbbf1afbc6a4026f4cc2f1d6eccbf5d2ba77c
Xander Benson <xanderbenson1421@yahoo.com>
2024-11-02 12:15:58 -0400

    Initial

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#inital
+#- Encrypt the password with a salt value
+
+#- Store the password in text file 
+#- Comma separated like (hashed value, salt value)
+
+
+#- Authenticate that the password can be retrieved
+#- Verification
+
+#- The menu options for encrption, decryptions, and closing (exiting) the program
+#- Run in some kind of loop so the program doesn't have to relaunch every time.
